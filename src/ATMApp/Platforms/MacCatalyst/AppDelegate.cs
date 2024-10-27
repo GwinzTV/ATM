cff23fb02981fc12a716a5fc1a33c6fe288191d2
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-10-27 20:49:08 +0000

    app_template_setup

---
+++ src/ATMApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ATMApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
